480b4142de156cf46534857aa77076d21e4d4b00
didierlechenne <didier@lechenne.fr>
2025-10-17 19:15:51 +0200

    update

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,33 +1,13 @@
-<?php
-/*
-  Templates render the content of your pages.
+<?php 
+$bgImage = $site->home_cover()->toFile() 
+  ? $site->home_cover()->toFile()->url() 
+  : 'assets/css/sq001.jpg';
 
-  They contain the markup together with some control structures
-  like loops or if-statements. The `$page` variable always
-  refers to the currently active page.
-
-  To fetch the content from each field we call the field name as a
-  method on the `$page` object, e.g. `$page->title()`.
-
-  This home template renders content from others pages, the children of
-  the `photography` page to display a nice gallery grid.
-
-  Snippets like the header and footer contain markup used in
-  multiple templates. They also help to keep templates clean.
-
-  More about templates: https://getkirby.com/docs/guide/templates/basics
-*/
-
+snippet('header-home', ['bgImage' => $bgImage]);
 ?>
-<?php snippet('header-home') ?>
 
 <div class="container">
   <?php snippet('nav') ?>
 </div>
 
-
-
-
-
-
-
+<?php snippet('footer') ?>
